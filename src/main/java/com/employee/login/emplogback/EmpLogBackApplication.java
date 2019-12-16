06d98e013e05b1b2d26e72904c06443ab8ce7927
devindianushika <devindianushika@gmail.com>
2019-12-16 21:38:03 +0530

    first commit

---
+++ src/main/java/com/employee/login/emplogback/EmpLogBackApplication.java
@@ -0,0 +1,13 @@
+package com.employee.login.emplogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpLogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpLogBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/employee/login/emplogback/EmpLogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.login.emplogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpLogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
